2f9db72aec949f0aaa93c0a0d788040d1cd35bb0
shinea <shinea_dd@163.com>
2024-06-07 23:11:59 +0800

    feat(): delete

--- create.js
+++ create.js
@@ -5,18 +5,20 @@ const path = require('path');
 const directoryPath = './';
 
 // Specify the number of files to create
-const numFiles = 200;
+const numFiles = 20;
 
 // Function to create a single file
-function createFile(fileName) {
+function createFile(fileName, txtData) {
     
   const filePath = path.join(directoryPath, fileName);
-  fs.writeFileSync(filePath, ''); // Create an empty file
+  fs.writeFileSync(filePath, txtData); // Create an empty file
+
+
 }
 
 // Loop to create multiple files
-for (let i = 120; i <= numFiles; i++) {
-  const fileName = `t${100 + i}.txt`;
-  createFile(fileName);
+for (let i = 1; i <= numFiles; i++) {
+  const fileName = `f-${100 + i}.txt`;
+  createFile(fileName, `2023-1-${i}`);
   console.log(`File "${fileName}" created.`);
 }
